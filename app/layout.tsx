16e87be386bcecefe71b07c7144b6f135f2f31cf
bryan-mcneil <bmcneil.t@gmail.com>
2026-07-17 12:57:41 -0600

    Dark mode Forced

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,6 +51,7 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
+          enableSystem={false}
           disableTransitionOnChange
         >
           <Navbar />
